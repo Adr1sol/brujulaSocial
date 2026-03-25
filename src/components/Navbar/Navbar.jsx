f2a19f70bb91068e9a2ca70413972462a2c2ec02
R1ve3raaTech <criverafwdcostarica@gmail.com>
2026-03-25 15:36:39 -0600

    commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,29 @@
+// components/Navbar/Navbar.jsx
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logoSinNombre from '../../images/logoSinNombre.png';
-import styles from './Navbar.module.css';
-
-const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <Link to="/" className={styles['nav-logo-container']}>
-        <img src={logoSinNombre} alt="Brújula Social Logo" className={styles['navbar-logo']} />
-        <span className={styles['nav-logo']}>Brújula Social</span>
-      </Link>
-      <ul className={styles['nav-links']}>
-        <li><Link to="/">Inicio</Link></li>
-        <li><Link to="/login">Iniciar Sesión</Link></li>
-        <li><Link to="/registro">Registro</Link></li>
-        <li><Link to="/registro">Contacto</Link></li>
-      </ul>
-    </nav>
-  );
-};
-
+import { Link } from "react-router-dom"
+import "../Navbar/NavBar.css"
+import logo from '../../assets/logo.png';
+ 
+function Navbar() {
+    return (
+      <>
+        <header className="navbar">
+          <div className="navbar-logo">
+            <a href="/">
+              <img src={logo} alt="Brújula Social Logo" className="logo-image" />
+            </a>
+            <a href="/">
+              <span className="logo-text-gradient">BRÚJULA SOCIAL</span>
+            </a>
+          </div>
+          <nav className="navbar-links">
+            <a href="#about">Sobre Nosotros</a>
+            <a href="/inicio">Inicia Sesión</a>
+            <a href="#register" className="btn-register">Registro</a>
+            <a href="#contact">Contacto</a>
+          </nav>
+        </header>
+      </>
+    );
+}
 export default Navbar;
